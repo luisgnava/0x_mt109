63f68d7e262078a3145da805d67aec545a37e5bd
Luis Nava <luis.gnava85@gmail.com>
2021-02-16 06:10:12 -0600

    created branch backend, added tvseries-queries.sql to the project

---
+++ tvseries-queries.sql
@@ -0,0 +1,8 @@
+INSERT INTO Actors (LastName, FirstName)
+VALUES ('Cardinal','Tom Erichsen');
+
+INSERT INTO Directors (LastName, FirstName)
+VALUES ('Thomas','Bill');
+
+INSERT INTO TVShows (NameShow, Director, Actors)
+VALUES ('Dance, Baby!','Bill Thomas','Cardinal Tom Erichsen');
