268272617ed4578a3ff75363c004b37c92f206bc
Luis Nava <luis.gnava85@gmail.com>
2021-02-16 06:32:32 -0600

    added new schema for database, changes made to tvseries-queries.sql

--- tvseries-queries.sql
+++ tvseries-queries.sql
@@ -1,3 +1,25 @@
+DROP DATABASE IF EXISTS directorsActors;
+
+CREATE DATABASE directorsActors;
+
+USE directorsActors;
+
+CREATE TABLE Actors (
+  LastName varchar(255),
+  FirstName varchar(255)
+);
+
+CREATE TABLE Directors (
+  LastName varchar(255),
+  FirstName varchar(255)
+);
+
+CREATE TABLE TVShows (
+  NameShow varchar(255),
+  Director varchar(255),
+  Actor varchar(255)
+);
+
 INSERT INTO Actors (LastName, FirstName)
 VALUES ('Cardinal','Tom Erichsen');
 
